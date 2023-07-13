656c88bbbe30f4151dbb1e8d72a62e610b5212b4
Hyeon-Uk <rlagusdnr120@gmail.com>
2023-07-13 23:02:46 +0900

    feature : TodoRepository의 테스트코드 작성
    save, findTodosWithCategoriesByMemberId 메서드에 대한 테스트코드 작성

---
+++ src/test/java/com/hyeonuk/todo/todo/repository/TodoRepositoryTest.java
@@ -0,0 +1,295 @@
+package com.hyeonuk.todo.todo.repository;
+
+import com.hyeonuk.todo.member.entity.Member;
+import com.hyeonuk.todo.member.repository.MemberRepository;
+import com.hyeonuk.todo.todo.entity.Category;
+import com.hyeonuk.todo.todo.entity.Todo;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.InvalidDataAccessApiUsageException;
+
+import java.util.*;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@DataJpaTest
+class TodoRepositoryTest {
+    @Autowired
+    private TodoRepository todoRepository;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+
+    private List<Member> memberList;
+    private List<Todo>[] todoList;
+    private List<Category>[] categoryList;
+
+    private final int MEMBER_SIZE = 15;
+
+
+    @BeforeEach
+    public void init() {
+        memberList = new ArrayList<>();
+        todoList = new ArrayList[MEMBER_SIZE];
+        categoryList = new ArrayList[MEMBER_SIZE];
+
+        for (int i = 0; i < MEMBER_SIZE; i++) {
+            todoList[i] = new ArrayList<>();
+            categoryList[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < MEMBER_SIZE; i++) {
+            Member member = Member.builder()
+                    .id("tester" + i)
+                    .name("tester" + i)
+                    .email("tester" + i + "@gmail.com")
+                    .password("1111")
+                    .build();
+
+            memberList.add(member);
+        }
+        memberRepository.saveAll(memberList);
+
+        for (int i = 0; i < 100; i++) {
+            int mIndex = i % memberList.size();
+            Member member = memberList.get(mIndex);
+
+            Category category = Category.builder()
+                    .title("title" + i)
+                    .member(member)
+                    .build();
+            Todo todo = Todo.builder()
+                    .category(todoList[mIndex].size() < 2 ? null : category)//카테고리가 null인 todo를 넣기위해서
+                    .content(member.getName().concat("의 ").concat(Integer.toString(i).concat("번째 todo")))
+                    .member(member)
+                    .complete(todoList[mIndex].size() % 2 == 0 ? false : true)
+                    .build();
+
+            if (category != null) categoryList[mIndex].add(category);
+            todoList[mIndex].add(todo);
+        }
+        for (int i = 0; i < MEMBER_SIZE; i++) {
+            categoryRepository.saveAll(categoryList[i]);
+            todoRepository.saveAll(todoList[i]);
+        }
+    }
+
+    /**
+     *
+     * update 테스트
+     *
+     * */
+
+    /**
+     *
+     * delete 테스트
+     *
+     * */
+
+
+    /**
+     * save 테스트
+     */
+    @Nested
+    @DisplayName("Save Test")
+    public class SaveTest {
+
+        /**
+         * 성공케이스
+         * 1. category가 null인 todo v
+         * 2. category가 있는 todo v
+         */
+        @Nested
+        @DisplayName("success")
+        public class Success {
+            @Test
+            @DisplayName("1. category가 null인 todo save 성공")
+            public void nullCategoryTodoSaveTest() {
+                long beforeSize = todoRepository.count();
+                int mIndex = 0;
+                Member member = memberList.get(mIndex);
+
+                Todo todo = Todo.builder()
+                        .member(member)
+                        .category(null)
+                        .content("null category")
+                        .build();
+
+                todoRepository.save(todo);
+
+                List<Todo> all = todoRepository.findAll();
+
+                assertThat(all.size()).isEqualTo(beforeSize + 1);
+                assertThat(all.contains(todo)).isTrue();
+
+                //제대로 저장됐는지 확인
+                Optional<Todo> result = todoRepository.findById(todo.getId());
+                assertThat(result).isNotEmpty();
+
+                Todo findById = result.get();
+                assertAll("Assert Object",
+                        () -> assertThat(findById.getContent()).isEqualTo(todo.getContent()),
+                        () -> assertThat(findById.getCategory()).isNull(),
+                        () -> assertThat(findById.getMember().getId()).isEqualTo(member.getId())
+                );
+            }
+
+            @Test
+            @DisplayName("2. category가 있는 todo")
+            public void notNullCategoryTodoSaveTest() {
+                long beforeSize = todoRepository.count();
+                int mIndex = 0;
+                Member member = memberList.get(mIndex);
+
+                Category category = categoryList[mIndex].stream()
+                        .findAny()
+                        .get();//해당 유저의 카테고리중 null이 아닌것중 아무거나 하나
+                Todo todo = Todo.builder()
+                        .member(member)
+                        .category(category)
+                        .content("null category")
+                        .build();
+
+                todoRepository.save(todo);
+
+                List<Todo> all = todoRepository.findAll();
+
+                assertThat(all.size()).isEqualTo(beforeSize + 1);
+                assertThat(all.contains(todo)).isTrue();
+
+                //제대로 저장됐는지 확인
+                Optional<Todo> result = todoRepository.findById(todo.getId());
+                assertThat(result).isNotEmpty();
+
+                Todo findById = result.get();
+                assertAll("Assert Object",
+                        () -> assertThat(findById.getContent()).isEqualTo(todo.getContent()),
+                        () -> assertThat(findById.getCategory()).isEqualTo(category),
+                        () -> assertThat(findById.getMember().getId()).isEqualTo(member.getId())
+                );
+            }
+        }
+
+
+        /**
+         * 실패케이스
+         * 1. 존재하지 않는 category인 todo v
+         * 2. 존재하지 않는 member인 todo v
+         */
+        @Nested
+        @DisplayName("fail")
+        public class Fail {
+            @Test
+            @DisplayName("1. 존재하지 않는 category인 todo")
+            public void notExistCategoryTodoTest() {
+                //given
+                int mIndex = 0;
+                Member member = memberList.get(mIndex);//멤버는 존재하는걸로
+
+                Category notExistCategory = Category.builder()
+                        .id(categoryList[mIndex].size() + 1000l)//존재하지않는 카테고리id
+                        .title("notExistCategory")
+                        .build();//id값이 없는 카테고리 (존재하지 않는 카테고리)
+
+                Todo todo = Todo.builder()
+                        .category(notExistCategory)
+                        .member(member)
+                        .content("존재하지않는 투두!")
+                        .build();
+
+                //when & then
+                assertThrows(DataIntegrityViolationException.class, () -> {
+                    todoRepository.saveAndFlush(todo);
+                });
+            }
+
+            @Test
+            @DisplayName("2. 존재하지 않는 member인 todo")
+            public void notExistMemberTodoTest() {
+                //given
+                Member notExistMember = Member.builder()
+                        .id("notExistMember")
+                        .build();
+
+                Category category = categoryList[0].get(0);//카테고리는 존재하는걸로
+
+                Todo todo = Todo.builder()
+                        .category(category)
+                        .member(notExistMember)
+                        .content("존재하지않는 사용자!")
+                        .build();
+
+                //when & then
+                assertThrows(InvalidDataAccessApiUsageException.class, () -> {
+                    todoRepository.saveAndFlush(todo);
+                });
+            }
+        }
+    }
+
+
+    /**
+     * memberId를 이용하여 todos를 불러옴. complete asc, createdAt desc
+     */
+    @Nested
+    @DisplayName("findTodosWithCategoriesByMemberId")
+    public class FindTodosWithCategoriesByMemberIdTest {
+
+        /**
+         * 성공케이스
+         * 1. 저장된 모든 Todo들을 불러옴 v
+         */
+        @Nested
+        @DisplayName("success")
+        public class Success {
+            @Test
+            @DisplayName("1. 저장된 모든 Todo들을 불러옴")
+            public void findAllSuccess() {
+                for (int i = 0; i < MEMBER_SIZE; i++) {
+                    Member member = memberList.get(i);
+
+                    List<Todo> todos = todoRepository.findTodosWithCategoriesByMemberId(member.getId());
+
+                    assertThat(todos.size()).isEqualTo(todoList[i].size());//사이즈가 일치해야함
+
+                    for (int j = 0; j < todos.size(); j++) {
+                        Todo todo = todos.get(j);
+                        assertThat(todoList[i].contains(todo)).isTrue();
+                    }
+                }
+            }
+        }
+
+        /**
+         * 실패 케이스
+         * 1. 없는 Id로 불러옴 v
+         */
+        @Nested
+        @DisplayName("failure")
+        public class Failure {
+            @Test
+            @DisplayName("1. 없는 Id로 불러옴")
+            public void notExistMember() {
+                Member notExistMember = Member.builder()
+                        .id("notExistUser")
+                        .build();
+
+                List<Todo> todos = todoRepository.findTodosWithCategoriesByMemberId(notExistMember.getId());
+
+                assertThat(todos.isEmpty()).isTrue();
+            }
+        }
+    }
+
+}
