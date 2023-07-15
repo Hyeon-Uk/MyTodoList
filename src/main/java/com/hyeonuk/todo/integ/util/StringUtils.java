716927430e6fca24e0e3e1665076a7a16867aaa7
Hyeon-Uk <rlagusdnr120@gmail.com>
2023-07-16 00:57:48 +0900

    feat : randomString 메서드 추가
    param:
    - length : 만들고싶은 string의 길이
    
    return:
    - length만큼의 랜덤 문자열

--- src/main/java/com/hyeonuk/todo/integ/util/StringUtils.java
+++ src/main/java/com/hyeonuk/todo/integ/util/StringUtils.java
@@ -1,11 +1,26 @@
 package com.hyeonuk.todo.integ.util;
 
+import java.util.Random;
+
 public class StringUtils {
+    private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     public static boolean isNull(String str){
         return str==null;
     }
 
     public static boolean isBlank(String str){
         return isNull(str) || "".equals(str.trim());
+    }
+
+    public static String randomSting(int length){
+        Random random = new Random();
+        StringBuffer sb = new StringBuffer();
+
+        for(int i=0;i<length;i++){
+            int randomIndex = random.nextInt(CHARACTERS.length());
+            sb.append(CHARACTERS.charAt(randomIndex));
+        }
+
+        return sb.toString();
     }
 }
