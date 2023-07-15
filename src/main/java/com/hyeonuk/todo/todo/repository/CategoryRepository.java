912ed86f882828b4c346f38586dfab73ad2258e3
Hyeon-Uk <rlagusdnr120@gmail.com>
2023-07-16 00:58:22 +0900

    feat : findByMember 추가
    member엔티티를 이용하여 검색하는 메서드 추가

--- src/main/java/com/hyeonuk/todo/todo/repository/CategoryRepository.java
+++ src/main/java/com/hyeonuk/todo/todo/repository/CategoryRepository.java
@@ -1,7 +1,11 @@
 package com.hyeonuk.todo.todo.repository;
 
+import com.hyeonuk.todo.member.entity.Member;
 import com.hyeonuk.todo.todo.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CategoryRepository extends JpaRepository<Category,Long> {
+    List<Category> findByMember(Member member);
 }
